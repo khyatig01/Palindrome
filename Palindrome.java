1d232ec5d0ffdc105b173525da8120150a077533
Khyati Gupta <kgupta44@wisc.edu>
2025-11-20 20:29:40 -0500

    Pushing Palindrome Project

---
+++ Palindrome.java
@@ -0,0 +1,72 @@
+
+public class Palindrome {
+
+	/**
+	 * Recursively create a simple alphabet pattern, starting at the provided
+	 * character, moving backward to the beginning of the alphabet, and then forward
+	 * again to the provided letter, separating each letter with a space. This
+	 * method is only valid for capital letter input; if anything other than a
+	 * capital letter is provided as an argument, throw an IllegalArgumentException
+	 * with a descriptive error message.
+	 * 
+	 * @param start
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public static String mirrorA(char start) throws IllegalArgumentException {
+		return null;
+
+	}
+
+	/**
+	 * Recursively create an alphabet pattern, starting at the provided character,
+	 * and moving back and forth to the beginning of the alphabet by steps of size
+	 * step. This method is only valid for capital letter input; if anything other
+	 * than a capital letter is provided as an argument, throw an
+	 * IllegalArgumentException with a descriptive error message.
+	 * 
+	 * @param start
+	 * @param step
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public static String mirrorA(char start, int step) throws IllegalArgumentException {
+		return null;
+
+	}
+
+	/**
+	 * Recursively create a simple alphabet pattern, starting the provided
+	 * character, and moving forward to the end of the alphabet, and then backward
+	 * again to the provided letter, separating each letter with a space.This method
+	 * is only valid for capital letter input; if anything other than a capital
+	 * letter is provided as an argument, throw an IllegalArgumentException with a
+	 * descriptive error message.
+	 * 
+	 * @param start
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public static String mirrorZ(char start) throws IllegalArgumentException {
+		return null;
+
+	}
+
+	/**
+	 * Recursively create an alphabet pattern, starting at the provided character,
+	 * and moving forward and back to the end of the alphabet by steps of size step.
+	 * As before, the method is only valid for capital letter input and strictly
+	 * positive (not zero or negative) step sizes. For invalid input, throw an
+	 * IllegalArgumentException with a descriptive error message.
+	 * 
+	 * @param start
+	 * @param step
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public static String mirrorZ(char start, int step) throws IllegalArgumentException {
+		return null;
+
+	}
+
+}
---
+++ PalindromeTester.java
@@ -0,0 +1,40 @@
+
+public class PalindromeTester {
+
+	public static boolean testMirrorA() {
+		return false;
+
+	}
+
+	public static boolean testMirrorAStep() {
+		return false;
+
+	}
+
+	public static boolean testMirrorZ() {
+		return false;
+
+	}
+
+	public static boolean testMirrorZStep() {
+		return false;
+
+	}
+
+	/**
+	 * This method calls ALL of the test methods and return true if and only if all
+	 * methods return true.
+	 * 
+	 * @return
+	 */
+	public static boolean runAllTests() {
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
